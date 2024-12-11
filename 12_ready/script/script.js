93c400d6e8824c52901e2698c48f7001e6431c75
uxejune <148020148+uxejune@users.noreply.github.com>
2024-12-12 05:00:21 +0900

    first commit

---
+++ 12_ready/script/script.js
@@ -0,0 +1,5 @@
+const hoursElement = document.getElementsByClassName("hour_hand");
+const minutesElement = document.getElementsByClassName("minute_hand");
+const secondsElement = document.getElementsByClassName("second_hand");
+
+
